bcefa451a763aaec098971f49c3e4290867466ca
Bare Bears <Bare Bears>
2017-05-02 13:22:16 +0300

    Initial commit

---
+++ src/SignalmanPortal/Models/LearningViewModels/ArticlesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalmanPortal.Models.LearningViewModels
+{
+    public class ArticlesViewModel
+    {
+    }
+}
---
+++ src/SignalmanPortal/wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $('#main-navbar [title]').attr('data-toggle', 'tooltip');
+    $('#main-navbar [title]').attr('data-placement', 'bottom');
+    $('[data-toggle="tooltip"]').tooltip();
+});
