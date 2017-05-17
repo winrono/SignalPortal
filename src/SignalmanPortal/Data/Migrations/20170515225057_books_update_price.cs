744d2f48ebbbbed51a3cd32184b40443ab5478e5
Bare Bears <Bare Bears>
2017-05-17 12:55:30 +0300

    Added some functionality for books section #2

---
+++ src/SignalmanPortal/Data/Migrations/20170515225057_books_update_price.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignalmanPortal.Data.Migrations
+{
+    public partial class books_update_price : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Books",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Books",
+                nullable: false);
+        }
+    }
+}
---
+++ src/SignalmanPortal/Data/Migrations/20170517000609_Books_ImageExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignalmanPortal.Data.Migrations
+{
+    public partial class Books_ImageExtension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageExtension",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageExtension",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Books",
+                nullable: true);
+        }
+    }
+}
