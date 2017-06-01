69e76b23264d8c689eab62e1b50ff3aeb21ccf98
Roman Nosovich <romanmail.by@gmail.com>
2017-06-01 11:07:29 +0300

    Small reset

--- src/SignalmanPortal/Controllers/BooksController.cs
+++ src/SignalmanPortal/Controllers/BooksController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,47 +20,41 @@ namespace SignalmanPortal.Controllers
             _environment = environment;
             _mapper = mapper;
         }
-        public IActionResult Index()
-        {
-            return View(_booksRepository.BookCategories);
-        }
 
-        public IActionResult Details(int id)
+        public IActionResult GetBookById(int id)
         {
-            return View(_booksRepository.GetBookById(id));
+            return new JsonResult(_booksRepository.GetBookById(id));
         }
 
         public IActionResult GetBooksForCategory(int? categoryId, int pageId, int itemsPerPage)
         {
-            if (itemsPerPage == 0)
-            {
-                itemsPerPage = 6;
-            }
-
-            var books = _booksRepository.Books;
-
-            if (categoryId != null)
-            {
-                books = books.Where(x => x.CategoryId == categoryId);
-            }
-
-            books = books.OrderByDescending(x => x.BookId).Skip(pageId *itemsPerPage).Take(itemsPerPage);
+            var books = _booksRepository.GetBooksPaginated(categoryId, pageId, itemsPerPage);
 
             return new JsonResult(books);
         }
 
         public IActionResult GetBookCategories()
         {
-            List<BookCategoryViewModel> categories = new List<BookCategoryViewModel>();
-            foreach (var bookCategory in _booksRepository.BookCategories)
-            {
-                categories.Add(_mapper.Map<BookCategory, BookCategoryViewModel>(bookCategory));
-            }
+            List<BookCategoryViewModel> categories = _mapper.Map<IEnumerable<BookCategory>, List<BookCategoryViewModel>>(_booksRepository.BookCategories);
 
             return new JsonResult(categories);
         }
 
-        public FileResult DownloadBook(int id, string fileExtension)
+        public IActionResult CheckFileExistance(int id, string fileExtension)
+        {
+            var fileName = id + fileExtension;
+            var filePath = _environment.WebRootPath + "\\data\\books\\" + id + fileExtension;
+            if (System.IO.File.Exists(filePath))
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public IActionResult DownloadBook(int id, string fileExtension)
         {
             var fileName = id + fileExtension;
             var filePath = _environment.WebRootPath + "\\data\\books\\" + id + fileExtension;
